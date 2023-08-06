316364b1bd88367cf3b2170ee4ae71edb3b2ec02
Oloishorua <jacoboloishorua@gmail.com>
2023-08-06 23:56:27 +0300

    nested for

--- Day003.js
+++ Day003.js
@@ -1,1 +1,11 @@
 // Nested Loops and scope
+// program that prints each combination of 2 dice rolls
+
+
+// outer loop
+for(let i=1; i<=6;i++){
+    // inner loop
+    for(let j=1;j<=6;j++){
+        console.log(i + " "+ j);
+    }
+}
