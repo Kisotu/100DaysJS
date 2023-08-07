f1f6a388dade19e34c25308da192d4273db55bec
Oloishorua <jacoboloishorua@gmail.com>
2023-08-07 23:58:18 +0300

    intro, accessing and modifying arrays

---
+++ Day004.js
@@ -0,0 +1,24 @@
+// All about Arrays, can be created in two ways, bracket [] notation or array constructor
+
+// [] notation
+
+const myArray = []; //create empty array
+
+const array2 = [2,4,5,7,8] //initialize some values
+
+
+// array constructor
+
+const array3 = new Array() //empty
+
+const array4 = new Array(4,5,6,7) //with values
+const books = new Array("Physics001", "Biology003", "Intro to Programming in C");
+
+
+// accessing array elements
+console.log(books[2])
+
+// modify elements
+
+books[1] = "Data Structures and Algorithms"
+console.log(books)
