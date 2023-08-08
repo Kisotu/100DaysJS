aabc8b2f8f932fa2ef96148a25108136540a832e
Oloishorua <jacoboloishorua@gmail.com>
2023-08-09 01:25:44 +0300

    higher order functions and Callbacks

---
+++ Day006.js
@@ -0,0 +1,6 @@
+// Higher Order Functions and Callbacks
+
+// functions that talke other functions as arguments or use them as retunrn values.
+// Used to manipulate arrays and simplify complex tasks.
+
+// Callback function is a funcrion passed to another function as an argument
