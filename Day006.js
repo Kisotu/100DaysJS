339d01590532c23b9a4b79a1e477067a1913fc96
Oloishorua <jacoboloishorua@gmail.com>
2023-08-09 23:53:15 +0300

    using HOFs and callbacks

--- Day006.js
+++ Day006.js
@@ -3,4 +3,12 @@
 // functions that talke other functions as arguments or use them as retunrn values.
 // Used to manipulate arrays and simplify complex tasks.
 
-// Callback function is a funcrion passed to another function as an argument
+// Callback function is a function passed to another function as an argument
+
+// map() built in HOF that takes a callback fxn as an argument.
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+const squared = numbers.map((number) => number * number)
+
+console.log(squared)
